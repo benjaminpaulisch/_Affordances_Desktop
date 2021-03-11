bc1113d9da40d5cd4dd872a4e97581b72dc985a8
ademosrakh <57294643+ademosrakh@users.noreply.github.com>
2021-03-11 18:25:28 +0100

    Update MovingAround.cs
    Kamerapositionsreset bei Leertaste eingebaut.

--- Assets/Scripts/MovingAround.cs
+++ Assets/Scripts/MovingAround.cs
@@ -16,40 +16,57 @@ public class MovingAround : MonoBehaviour
     // private float MouseY = 0.0f;
 
     // Start is called before the first frame update
+
+
+    private Vector3 startPosition;
+
+
+
+
     void Start()
     {
-        transform.position = new Vector3(-2.5f,PlayerHeight,0f);
+        transform.position = new Vector3(-2.5f, PlayerHeight, 0f);
         Turning = 180f;
         TurningSpeed = 100f;
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         //MovementX = Input.GetAxis ("Horizontal");
-        MovementY = Input.GetAxis ("Vertical");
+        MovementY = Input.GetAxis("Vertical");
 
-        if (MovementY != 0) 
+        if (MovementY != 0)
         {
             //transform.position += transform.right * MovementX * Time.deltaTime * MovingSpeed;
             transform.position += transform.forward * MovementY * Time.deltaTime * MovingSpeed;
             transform.position = new Vector3(transform.position.x, PlayerHeight, transform.position.z);
         }
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
             Turning += Time.deltaTime * TurningSpeed;
         }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
             Turning -= Time.deltaTime * TurningSpeed;
         }
         transform.eulerAngles = new Vector3(0.0f, Turning, 0.0f);
 
-        // if (Input.GetMouseButton(0)){
-        //     MouseX += MouseSpeed * Input.GetAxis("Mouse X");
-        //     MouseY += MouseSpeed * Input.GetAxis("Mouse Y");
-        //     transform.eulerAngles = new Vector3(MouseY, MouseX, 0.0f);
-        // }
-        
-    }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            {
+                transform.position = startPosition;
+            }
 
+            // if (Input.GetMouseButton(0)){
+            //     MouseX += MouseSpeed * Input.GetAxis("Mouse X");
+            //     MouseY += MouseSpeed * Input.GetAxis("Mouse Y");
+            //     transform.eulerAngles = new Vector3(MouseY, MouseX, 0.0f);
+            // }
+
+        }
+
+    }
 }
